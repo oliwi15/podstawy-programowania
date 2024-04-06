4a6e3a25278f7661a99430065fb507ed42e80f65
Oliwia Kowalczyk <oliwia.szelag@op.pl>
2024-04-06 11:24:13 +0200

    chore: init project

---
+++ cwiczenie_1.java
@@ -0,0 +1,9 @@
+public class cwiczenie_1 {
+
+    public static void main(String[] args) {
+        String name = "Oliwia Kowalczyk";
+        int nr_albumu = 51313;
+        System.out.println(name + " " + nr_albumu);
+
+    }
+}
